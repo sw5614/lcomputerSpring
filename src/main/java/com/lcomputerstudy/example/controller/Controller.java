2b00de4143593a2f397ff2bec5a463ce1f85056e
sw5614 <sw5614@paran.com>
2023-01-06 11:27:21 +0900

    0106 학원에서

--- src/main/java/com/lcomputerstudy/example/controller/Controller.java
+++ src/main/java/com/lcomputerstudy/example/controller/Controller.java
@@ -1,7 +1,11 @@
 package com.lcomputerstudy.example.controller;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
@@ -14,15 +18,18 @@ import com.lcomputerstudy.example.service.UserService;
 @org.springframework.stereotype.Controller
 public class Controller {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired UserService userservice;
 	@Autowired BoardService boardservice;
 	
 	@RequestMapping("/")
-	public String home(Model model) {
+	public String home(Model model) { //Model은 스프링 기능, key와 value로 이루어져있는 HashMap
 		
-		List<Board> list = boardservice.selectBoardList();
+		List<Board> list = boardservice.selectBoardList();  
 		model.addAttribute("list", list);
-
+		logger.debug("debug");
+	     logger.info("info");
+	     logger.error("error");
 		return "/index";
 	}
 	
@@ -42,7 +49,7 @@ public class Controller {
 	      user.setEnabled(true);
 	      user.setAccountNonLocked(true);
 	      user.setCredentialsNonExpired(true);
-	      user.setAuthorities(AuthorityUtils.createAuthorityList("ROLE_USER"));   
+	      user.setAuthorities(AuthorityUtils.createAuthorityList("ROLE_USER")); // 유저 권한세팅 ROLE_USER lc_auth에 저장    
 	      
 	      //유저 생성
 	      userservice.createUser(user);
@@ -50,12 +57,28 @@ public class Controller {
 	      userservice.createAuthorities(user);
 	      
 	      return "/login";
-	      
-	 
+	     
 	   }
-    @RequestMapping(value="/login")
+    @RequestMapping(value="/login")     
     public String beforeLogin(Model model) {
     	return "/login";
     }
+    @Secured({"ROLE_ADMIN"})
+	   @RequestMapping(value="/admin")
+	   public String admin(Model model) {
+	      return "/admin";
+	   }
+	   
+	   @Secured({"ROLE_USER"})
+	   @RequestMapping(value="/user/info")
+	   public String userInfo(Model model) {
+	      
+	      return "/user_info";
+	   }
+	   
+	   @RequestMapping(value="/denied")
+	   public String denied(Model model) {
+	      return "/denied";
+	   }
 }
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,4 +1,3 @@
-// index.jsp
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
@@ -13,13 +12,13 @@
      <h1>Home Page</h1>
         <hr>
         <div>
-           <sec:authorize access="isAnonymous()">
+           <sec:authorize access="isAnonymous()"> <!--로그인하지 않았을때 ( 권한없는 익명사용자일경우 isAnonymous()  ) -->
               <a href="/login">로그인</a>
-              <a href="/beforeSignup">회원가입</a>
+              <a href="/beforeSignUp">회원가입</a>
            </sec:authorize>   
-            <sec:authorize access="isAuthenticated()">
+            <sec:authorize access="isAuthenticated()"> <!--  로그인했을때 (권한 상관없이 로그인 인증받았을때isAuthenticated()) -->
                <a href="/logout">로그아웃</a>
-               <sec:authentication property="principal" var="principal"/>
+               <sec:authentication property="principal" var="principal"/> <!-- 데이터 값 가져올때  어디서왔을까? 게시물은뭘로?-->
                <h2>${principal }</h2>
             </sec:authorize>
             
